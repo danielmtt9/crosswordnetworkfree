943da303de0f0a866a10f4be65c2d967e5350dc9
Daniel Aroko <daniel.a@mapletynetechnologies.com>
2026-02-10 12:13:04 -0500

    Revert "chore: Hostinger startup defaults"
    This reverts commit cefab00353a3c021a2e8abed016a97f3dc8eb036.

--- app.js
+++
@@ -1,4 +0,0 @@
-// Hostinger Node.js / Passenger setups often default to `app.js` as the startup file.
-// Delegate to our actual custom Next server.
-require('./server.js');
-
--- index.js
+++
@@ -1,3 +0,0 @@
-// Some Node panels default to `index.js` as the startup file.
-require('./server.js');
-
